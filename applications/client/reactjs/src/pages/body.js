d5c40e2349e7228965e3f456838579223d178f34
pgautam54 <pritamgautam45@gmail.com>
2022-12-04 11:35:39 -0800

    bodysetup

--- applications/client/reactjs/src/pages/body.js
+++ applications/client/reactjs/src/pages/body.js
@@ -10,21 +10,20 @@ class Body extends React.Component {
           <div className = 'main'>
             <div className ='overlay'></div>
             <img src = {image} height = "100%" width ="100%" />
-            {/* <video src={videobg} autoPlay loop/> */}
+           
             
         
 
             <div className ="font-effect-neon">
 
                 <h1>Welcome to BabyBlackjack</h1> 
-                {/* < button className="signup" href="#link">Signup</button>
-                < button className="signin" href="#link">Signin</button> */}
+               
             </div>
             <Link to="/main">
           <button className="playbutton">Play as guest</button>
         </Link>
              
-            {/* <Card rank="K" suit="S" /><Card rank="B" suit="S" /> */}
+           
 
         </div>
 
@@ -38,3 +37,5 @@ class Body extends React.Component {
     }
 }
 export default Body;
+
+//Ready to push
