01856ec44bfbf622cc2276f18bcd92ac33f29871
Tyler9648 <tyler9648@outlook.com>
2022-12-07 18:58:16 -0800

    Final version

--- applications/client/reactjs/src/components/logout.js
+++ applications/client/reactjs/src/components/logout.js
@@ -7,7 +7,16 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 function NavbarLogout() {
-  
+  const logout = () => {
+  if(localStorage.getItem("authenticated")){
+  localStorage.setItem("authenticated".false);
+  localStorage.removeItem("user");
+  } else {
+  console.log("why are u trying to logout? youre not even logged in!");
+  }
+
+  }
+
         return(
  <div>
   <Navbar bg="dark" variant="dark" > 
@@ -21,7 +30,7 @@ function NavbarLogout() {
             
            
             <NavDropdown title="Menu" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/">Logout</NavDropdown.Item>
+              <NavDropdown.Item href="/" onClick={logout}>Logout</NavDropdown.Item>
               <NavDropdown.Item href="/">
                 
               </NavDropdown.Item>
